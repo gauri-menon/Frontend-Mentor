6d26208b9eec241a9fc48baede64aed1db1e5d93
gauri-menon <122345071+gauri-menon@users.noreply.github.com>
2023-04-05 18:35:21 +0530

    Add files via upload
    Day 2 commit

---
+++ starter-code/function.js
@@ -0,0 +1,16 @@
+function openMenu() {
+    document.getElementsByClassName("navbar")[0].style.display="flex";
+    document.getElementsByTagName("hr")[0].style.display="flex";
+    // document.getElementsByClassName("navbar")[0].style.flexDirection="column";
+    document.getElementsByClassName("flex-nav")[0].style.justifyContent="flex-end";
+    // document.getElementsByClassName("flex-nav")[0].style.flexDirection="column";
+    document.getElementsByClassName("menu-open")[0].style.display="none";
+    document.getElementsByClassName("menu-close")[0].style.display="initial";
+}
+
+function closeMenu() {
+
+    document.getElementsByClassName("menu-close")[0].style.display="none";
+    document.getElementsByClassName("menu-open")[0].style.display="initial";
+    document.getElementsByClassName("navbar")[0].style.display="none";
+}
