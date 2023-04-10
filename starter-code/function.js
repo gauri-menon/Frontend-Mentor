5446e6c9c8f2296923e358ce9f5680e6fb3f35b9
gauri-menon <122345071+gauri-menon@users.noreply.github.com>
2023-04-10 18:25:27 +0530

    Add files via upload
    Day 5- Completed with background images

--- starter-code/function.js
+++ starter-code/function.js
@@ -1,9 +1,7 @@
 function openMenu() {
     document.getElementsByClassName("navbar")[0].style.display="flex";
     document.getElementsByTagName("hr")[0].style.display="flex";
-    // document.getElementsByClassName("navbar")[0].style.flexDirection="column";
     document.getElementsByClassName("flex-nav")[0].style.justifyContent="flex-end";
-    // document.getElementsByClassName("flex-nav")[0].style.flexDirection="column";
     document.getElementsByClassName("menu-open")[0].style.display="none";
     document.getElementsByClassName("menu-close")[0].style.display="initial";
 }
@@ -16,5 +14,17 @@ function closeMenu() {
 }
 
 function validateForm() {
-    var x=document.getElementsByTagName("form");
+    var input_obj=document.getElementsByClassName("field-input");
+    for(var i=0;i<5;i++) {
+        if(!input_obj[i].checkValidity()) {
+            document.getElementsByClassName("missing-field")[i].innerHTML="This field can't be empty";
+            document.getElementsByTagName("label")[i].style.color="#FF0000";
+            document.getElementsByClassName("field-input")[i].style.borderBottomColor="#FF0000";
+            break;
+        }
+        else {
+            document.getElementsByClassName("missing-field")[i].innerHTML="";
+            document.getElementsByTagName("label")[i].style.color="#36536B";
+        }
+    }
 }
